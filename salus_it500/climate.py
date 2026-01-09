462697d0f2240ab21b6f7f64028e412aab4821ed
Lukasz Adamczyk <ladamczyk.it@gmail.com>
2026-01-09 21:27:53 +0100

    chore: adjust

--- salus_it500/climate.py
+++ salus_it500/climate.py
@@ -50,11 +50,12 @@ MAX_TEMP = 34.5
 SUPPORT_FLAGS = ClimateEntityFeature.TARGET_TEMPERATURE
 
 # def setup_platform(hass, config, add_entities, discovery_info=None):
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):    
-    name = "Salus Thermostat"
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
-    id = config.get(CONF_ID)
+async def async_setup_platform(hass, hass_config, async_add_entities, discovery_info=None):    
+    name = "Salus thermostat"
+
+    username = discovery_info[CONF_USERNAME]
+    password = discovery_info[CONF_PASSWORD]
+    id = discovery_info[CONF_ID]
 
     async_add_entities(
         [SalusThermostat(hass, name, username, password, id)]
--- salus_it500/water_heater.py
+++ salus_it500/water_heater.py
@@ -38,11 +38,12 @@ _LOGGER = logging.getLogger(__name__)
 SUPPORT_FLAGS = WaterHeaterEntityFeature.OPERATION_MODE
 
 # def setup_platform(hass, config, add_entities, discovery_info=None):
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):    
+async def async_setup_platform(hass, hass_config, async_add_entities, discovery_info=None):    
     name = "Salus water heater"
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
-    id = config.get(CONF_ID)
+
+    username = discovery_info[CONF_USERNAME]
+    password = discovery_info[CONF_PASSWORD]
+    id = discovery_info[CONF_ID]
 
     async_add_entities(
         [SalusWaterHeater(hass, name, username, password, id)]
